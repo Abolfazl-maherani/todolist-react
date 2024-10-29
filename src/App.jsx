3ec34e8757bf42601706c658ab04f578cae0d932
dev.Abolfazl <mahrani.prj@gmail.com>
2024-10-30 00:47:18 +0330

    feature: Add input and setup that for add task to board

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,13 @@ import Dialog from "./components/Ui/Dialog";
 
 function App() {
 
-    const [boards, setBoards] = useState([
-        {
-            tasks: [],
-            boardName: 'Todo'
-        }
-    ]);
+    const [boards, setBoards] = useState([{
+        tasks: [], boardName: 'Todo'
+    }]);
     const [addBoardDialog, setAddBoardDialog] = useState(false);
     const [boardNameInput, setBoardNameInput] = useState("");
+    const [selectInputTask, setSelectInputTask] = useState(boards[0]?.boardName);
+    const [input, setInput] = useState('')
     const [editInfo, setEditInfo] = useState({
         index: null,
 
@@ -23,7 +22,7 @@ function App() {
     const addBoard = (newBoardName) => {
         setBoards([...boards, {tasks: [], boardName: newBoardName}]);
     };
-
+    const clearInput = () => setInput('')
     // Function to add a task to a specific board
     const addTaskToBoard = (boardIndex, newTask) => {
         const updatedBoards = boards.map((board, index) => {
@@ -37,9 +36,11 @@ function App() {
     const removeTaskOnBoard = (boardIndex, taskIndex) => {
         const updatedBoards = boards.map((board, index) => {
             if (index === boardIndex) {
-                return {...board, tasks: board.tasks.filter((_,index)=>{
-                    return index !== taskIndex;
-                    })};
+                return {
+                    ...board, tasks: board.tasks.filter((_, index) => {
+                        return index !== taskIndex;
+                    })
+                };
             }
             return board;
         });
@@ -49,23 +50,15 @@ function App() {
     const isEditableMode = useMemo(() => {
         return editInfo.index !== null
     }, [editInfo])
-    const submitTaskHandler = (e, val, indexBoard, setInput) => {
+    const submitTaskHandler = (e) => {
         e.preventDefault()
+        if (input?.trim()) {
+            const indexOfBoard = boards?.findIndex(item => item.boardName === selectInputTask)
+            if (indexOfBoard !== -1) {
+                addTaskToBoard(indexOfBoard, input);
+                clearInput()
 
-        if (val?.trim()) {
-            if (isEditableMode) {
-                // const taskCopy = [...tasks]
-                // taskCopy[editInfo.index] = val
-                // setTasks(taskCopy)
-                // setInput('')
-                // setEditInfo({
-                //     index: null
-                // })
-            } else {
-                addTaskToBoard(indexBoard, val);
-                setInput('')
             }
-
         }
     }
     const editTaskOnBoardHandler = (boardIndex, taskIndex, setInput) => {
@@ -86,62 +79,77 @@ function App() {
             setAddBoardDialog(false)
         }
     }
-    return (
-        <>
-            <main className="p-10">
-                <BaseButton
-                    className="mb-3
-                " type="success" $attrs={{
-                    type: 'button',
-                    onClick: () => {
-                        setAddBoardDialog(true)
-                    }
-                }}
-                >Create Board</BaseButton>
-                <div className=" grid gap-3 grid-cols-4">
-                    {
-                        boards?.length && boards.map((board, indexBoard) => {
-                            return <CardTask editable={editInfo.index !== null}
-                                             submitTaskHandler={(e, val, setInput) => submitTaskHandler(e, val, indexBoard, setInput)}
-                                             title={board.boardName}>
-                                {
-                                    ({inputText, setInput}) => board?.tasks?.length ?  board.tasks?.map((item, index) => <CardTaskItem
-                                        editHandler={() => editTaskOnBoardHandler(indexBoard, index, setInput)}
-                                        removeHandler={() => removeTaskOnBoard(indexBoard, index)}
-                                        order={index + 1} key={index} title={item}/>) : <div className="w-full h-full flex justify-center items-center text-[#444]">Empty task...</div>
-                                }
-                            </CardTask>
-                        })
+    return (<>
+        <main className="p-10">
+            <BaseButton className="mb-3  " type="success" $attrs={{
+                type: 'button', onClick: () => {
+                    setAddBoardDialog(true)
+                }
+            }}>Create Board</BaseButton>
+            <div className=" grid gap-3 grid-cols-4">
+                {boards?.length && boards.map((board, indexBoard) => {
+                    return <CardTask editable={editInfo.index !== null}
+                                     title={board.boardName}>
+                        {({inputText, setInput}) => board?.tasks?.length ? board.tasks?.map((item, index) =>
+                                <CardTaskItem
+                                    editHandler={() => editTaskOnBoardHandler(indexBoard, index, setInput)}
+                                    removeHandler={() => removeTaskOnBoard(indexBoard, index)}
+                                    order={index + 1} key={index} title={item}/>) :
+                            <div className="w-full h-full flex justify-center items-center text-[#444]">Empty
+                                task...</div>}
+                    </CardTask>
+                })
 
-                    }
+                }
 
 
-                </div>
-            </main>
-            {addBoardDialog && <Dialog onClose={() => setAddBoardDialog(false)}>
-                <div className="max-w-[80vw] w-[350px] rounded-xl flex justify-center items-center bg-white py-20">
-                    <form onSubmit={submitBoardHandler} className="flex flex-col">
-                        <input
-                            autoFocus
-                            value={boardNameInput}
-                            onChange={(e) => {
-
-                                setBoardNameInput(e.target.value)
-                            }}
-                            placeholder="write board name.."
-                            type="text" className="w-full bg-[#eee] rounded-[8px] p-2"/>
-
-                        <BaseButton $attrs={{
-                            type: 'submit'
-                        }} className="mx-auto mt-5 inline-block" type="success">
-                            Create Board
-                        </BaseButton>
+            </div>
+            <div className="fixed bottom-3 left-1/2 transform -translate-x-1/2 w-full max-w-xl mx-auto ">
+                <div className="flex w-full gap-2">
+                    <form onSubmit={submitTaskHandler} className="flex w-full gap-2">
+                        <div className="relative shadow-md bg-[#eee] px-2 rounded-[8px]   w-full flex">
+                            <input value={input} onChange={(e) => setInput(e.target.value)} placeholder="write task..."
+                                   type="text"
+                                   className="w-full outline-0 bg-transparent py-2 "/>
+                            <hr className="w-[1px] h-[80%] my-1 mx-1 bg-[#bbb]"/>
+                            <select
+                                value={selectInputTask} onChange={(e) => setSelectInputTask(e.target.value)}
+                                className=" bg-transparent outline-0 max-w-[80px] " name=""
+                                id="">
+                                {boards.map((board, index) => (
+                                    <option key={index} value={index}>{board.boardName}</option>))}
+                            </select>
+                        </div>
+                        <button
+                            className="bg-green-800 shadow-md inline-flex items-center justify-center px-3 rounded-[8px] text-white text-[13px]">Submit
+                        </button>
                     </form>
                 </div>
+            </div>
+        </main>
+        {addBoardDialog && <Dialog onClose={() => setAddBoardDialog(false)}>
+            <div className="max-w-[80vw] w-[350px] rounded-xl flex justify-center items-center bg-white py-20">
+                <form onSubmit={submitBoardHandler} className="flex flex-col">
+                    <input
+                        autoFocus
+                        value={boardNameInput}
+                        onChange={(e) => {
 
-            </Dialog>}
-        </>
-    )
+                            setBoardNameInput(e.target.value)
+                        }}
+                        placeholder="write board name.."
+                        type="text" className="w-full bg-[#eee] rounded-[8px] p-2"/>
+
+                    <BaseButton $attrs={{
+                        type: 'submit'
+                    }} className="mx-auto mt-5 inline-block" type="success">
+                        Create Board
+                    </BaseButton>
+                </form>
+            </div>
+
+        </Dialog>}
+    </>)
 }
 
 export default App;
--- src/components/CardTask.jsx
+++ src/components/CardTask.jsx
@@ -1,46 +1,24 @@
-import {type} from "@testing-library/user-event/dist/type";
 import {useState} from "react";
 
 
 const CardTask = ({title, children, submitTaskHandler, editable}) => {
     const [inputText, setInput] = useState()
 
-    return (
-        <>
-            <div
-
-                className="border-[4px] flex flex-col relative h-[500px] rounded-[8px] bg-[#F9F3E5] border-black border-solid">
-                <div className="border-b-[4px] px-4 py-4 border-black border-solid">
-                    <h4  className="text-[32px] truncate text-center font-bold text-black ">
-                        {title}
-                    </h4>
-                </div>
-                <div className="px-4  flex-grow capitalize font-medium overflow-y-auto py-[16px]">
-
-                    {children({inputText, setInput})}
-                </div>
-
-                <section className="px-4  mb-1  py-1 w-full ">
-                    <div className="">
-                        <form className="w-full flex gap-2 "
-                              onSubmit={(e) => submitTaskHandler(e, inputText, setInput)}>
-                            <input
-                                value={inputText}
-                                onChange={({target}) => setInput(target.value)} placeholder={`Write to ${title}...`} type="text"
-                                className="grow px-2 rounded-[8px] "/>
-                            <button disabled={!inputText?.trim()} type="submit"
-
-                                    className={`text-white bg-green-800 rounded-[8px] py-1 px-3 ${!inputText?.trim() ? 'opacity-80 cursor-no-drop' : ''}`}
-                            >
-                                {editable ? 'Edit' : 'Submit'}
-                            </button>
-                        </form>
-                    </div>
-                </section>
+    return (<>
+        <div
+            className="border-[4px] flex flex-col relative h-[500px] rounded-[8px] bg-[#F9F3E5] border-black border-solid">
+            <div className="border-b-[4px] px-4 py-4 border-black border-solid">
+                <h4 className="text-[32px] truncate text-center font-bold text-black ">
+                    {title}
+                </h4>
             </div>
+            <div className="px-4  flex-grow capitalize font-medium overflow-y-auto py-[16px]">
 
-        </>
-    )
+                {children({inputText, setInput})}
+            </div>
+        </div>
+
+    </>)
 }
 
 export default CardTask;
